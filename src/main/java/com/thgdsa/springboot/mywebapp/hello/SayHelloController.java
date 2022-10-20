1b39f1ecb71ff4af7eeab56b6ac8d9d5ab4496a4
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-10-20 15:11:11 -0300

    Step 4 - Html page

--- src/main/java/com/thgdsa/springboot/mywebapp/hello/SayHelloController.java
+++ src/main/java/com/thgdsa/springboot/mywebapp/hello/SayHelloController.java
@@ -14,5 +14,22 @@ public class SayHelloController {
 	public String sayHello() {
 		return "Hello! What are you learning today?";
 	}
+	
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My first HTML page - Changed </title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body - Changed");
+		sb.append("</body>");
+		sb.append("</html>");	
+		return sb.toString();
+	
+	}
 
 }
