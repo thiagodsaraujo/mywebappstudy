7654adff7e726c3e854414c79041387ed2051931
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-10-29 14:55:14 -0300

    Step 35 - 01 - Configuring Spring Security to Get H2 console Working

--- src/main/java/com/thgdsa/springboot/mywebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/thgdsa/springboot/mywebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.thgdsa.springboot.mywebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import java.util.function.Function;
 
@@ -45,6 +49,25 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+
+    //All URLs are protected
+    // A login form is shown for unauthorized requests
+    // CSRF disable
+    // frames
+    // Securityfilter chain: defines a filter chain matched against every request
+    // No método abaixo quer garantir que todas as solicitações sejam autenticadas
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
     }
 
 }
