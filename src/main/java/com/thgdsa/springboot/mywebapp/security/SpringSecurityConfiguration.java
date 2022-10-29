c3e719800fa203d2ca2dd8fc3beaed58ef29f300
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-10-29 09:13:09 -0300

    Step 31 - Configuring Spring Security with Custom User and Password Encoder

---
+++ src/main/java/com/thgdsa/springboot/mywebapp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.thgdsa.springboot.mywebapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    //LDAP or DataBase
+    //In Memory
+
+    //InMemoryUserDetailsManager
+    //InMemoryUserDetailsManager(UserDetails... users)
+
+
+
+    @Bean
+    public InMemoryUserDetailsManager creUserDetailsManager(){
+
+        Function<String, String> passwordEncoder=
+                input -> passwordEncoder().encode(input);
+
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username("Tita")
+                .password("123")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
